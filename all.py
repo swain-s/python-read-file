61c87672ec27d9759c7ac0d37f3c9d03e514e30a
swain <shenkun@bupt.edu.cn>
2020-10-19 00:49:19 +0800

    add test class

--- all.py
+++ all.py
@@ -1,6 +1,60 @@
 import sys
 import os
 
+import os
+import sys
+
+'''
+file - [cmd_list] - [runner] - [result_list]
+'''
+
+class CmdList(object):
+    def __init__(self):
+        self.cmd_file = []
+        self.cmd_cnt = 0
+        self.cmd_list = []
+
+    def read_cmd(self, file_name):
+        if file_name[-1-4, -1] is ".sql":
+            self._read_sql_cmd(file_name)
+        else:
+            self._read_shell_cmd(file_name)
+
+    def _read_sql_cmd(self, file_name):
+        for line in open(file_name):
+            if line.strip()[0] != '#':
+                sh = self._trans_sql_to_sh(line)
+                if sh:
+                    self.cmd_list.append(sh)
+
+    def _read_shell_cmd(self, file_name):
+        for line in open(file_name):
+            if line.strip()[0] != '#':
+                self.cmd_list.append(line)
+
+    def _trans_sql_to_sh(self, sql):
+        if len(sql) < 3:
+            return None
+
+        if sql[0:2] is r'\!':
+            return sql.strip[2:]
+
+        return None
+
+    def _print_cmd(self):
+        for cmd in self.cmd_list:
+            print(cmd)
+
+class ResultList(object):
+    def __init__(self):
+        self.result_cnt = 0
+        self.result_list = []
+
+class RunTestCast(object):
+    def __init__(self):
+        self.cmd_list = CmdList()
+
+
 class Event(object):
     def __init__(self):
         self.sender_id = ""
